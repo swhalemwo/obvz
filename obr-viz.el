4be6f0633713be0d97ea7ffa80e2daa096e72f0d
johannes ae <jojo.ae@hotmail.de>
2020-02-09 15:05:05 +0100

    cleanup code

--- obr-viz.el
+++ obr-viz.el
@@ -1,5 +1,341 @@
 (require 'cl)
+(require 'zmq)
 
+
+(defun obvis-create-children-links (parent child)
+    """basic hierarchical function"""
+    (if (> (length parent) 4)
+	    (when (not (equal (substring parent 0 4) "cls_"))
+		(concat parent " -- " child " -- isa"))
+	(concat parent " -- " child " -- isa"))
+    )
+
+
+(defun loop-over-upper-level-nodes (parent)
+    """get nodes and hierarchical links of parent node"""
+    ;; needs both nodes and links for some tests later iirc
+    (let ((childrenx (org-brain-children parent))
+	  (res ()
+	       ))
+	(push childrenx res)
+	
+	(push (mapcar (lambda (child)
+			  (funcall #'obvis-create-children-links parent child))
+		      childrenx) res)
+	;; can also push to nodes/links to function-scoped let variables
+	res
+	))
+
+
+(defun children-specific-depth-let (node depth)
+    (let ((nodes-upper-level (list node))
+	  (all-nodes (list node))
+	  (all-links ())
+	  (temp-res ())
+	  (all-res ())
+	  )
+
+	;; for each depth step
+	(dotimes (i depth)
+
+	    ;; temp-res: contains 2 lists (nodes/links) for each upper-level-node
+	    ;; idk it's a bit ugly
+	    ;; but idk how to work without temp-res
+	    (push (mapcar 'loop-over-upper-level-nodes nodes-upper-level) temp-res)
+
+
+	    ;; push nodes/links to local vars
+	    (mapc (lambda (nodex) (push nodex all-nodes)) (flatten-list (mapcar 'cdr (car temp-res))))
+	    (mapc (lambda (linkx) (push linkx all-links)) (flatten-list (mapcar 'car (car temp-res))))
+	    
+	    (setq nodes-upper-level (flatten-list (mapcar 'cdr (car temp-res))))
+	    (setq temp-res ())
+
+	    )
+	(push all-links all-res)
+	(push all-nodes all-res)
+
+	all-res
+	 
+	)
+    )
+
+
+(defun get-friend-links (nodes)
+    """get links between nodes"""
+    (setq nodes-backup nodes)
+
+    (setq friend-links ())
+    (setq friend-nodes ())
+
+    (while nodes
+	(setq node (car nodes))
+	(setq friends (org-brain-friends node))
+	(while friends
+	    (setq friend (car friends))
+	    
+	    (setq edge-annot (org-brain-get-edge-annotation node friend))
+	    (if (not (equal edge-annot nil))
+		    (progn
+			(push (concat node " -- " friend " -- " edge-annot) friend-links)
+			(push friend friend-nodes)
+			)
+		)
+
+	    (setq friends (cdr friends))
+
+	    )
+	(setq nodes (cdr nodes))
+	)
+
+    (setq res())
+    (push friend-links res)
+    (push friend-nodes res)
+    ;; friend-links
+    res
+    )
+
+	
+
+(defun obr-viz ()
+    (interactive)
+    """main function"""
+      ;; (mapcar 'children-specific-depth org-brain-pins 3)
+    (setq rel-nodes org-brain-pins)
+      
+    (setq total-nodes ())
+    (setq total-links ())
+
+    ;; get hierarchical relations
+
+    (while rel-nodes
+	(setq rel-node (car rel-nodes))
+
+	(setq node-res (children-specific-depth-let rel-node 8))
+	(push (car node-res) total-nodes)
+	(push (cdr node-res) total-links)
+
+	(setq rel-nodes (cdr rel-nodes))
+	)
+
+    (setq uniq-nodes-prep (counter (flatten-list total-nodes)))
+    (setq uniq-nodes (mapcar 'car uniq-nodes-prep))
+
+    ;; handle links
+    (setq friend-res (get-friend-links uniq-nodes))
+    (setq friend-links (cdr friend-res))
+    (push friend-links total-links)
+
+    ;; handle nodes
+    (setq friend-nodes (car friend-res))
+    (setq uniq-nodes (remove-duplicates (flatten-list (list uniq-nodes friend-nodes))))
+    (setq all-links (flatten-list total-links))
+
+    (setq node-string (mapconcat 'identity uniq-nodes ";"))
+    (setq link-string (mapconcat 'identity all-links ";"))
+
+    ;; include (or not) node texts
+    (if (equal include-node-texts t)
+	    (progn
+		(setq node-texts (mapcar 'org-brain-text uniq-nodes))
+		(setq node-text-alist (mapcar* #'cons uniq-nodes node-texts))
+		)
+	(setq node-text-alist (mapcar* #'cons uniq-nodes (make-list (len uniq-nodes) "")))
+	)
+
+    (setq graph-dict
+	  `(("links" . ,all-links)
+	    ("cur_node" . ,(org-brain-entry-at-pt))
+	    ("node_texts" . ,node-text-alist)
+	    )
+	  )
+    graph-dict
+    )
+    
+;; data structure: dict with key node, value text
+;; is itself dict in graph_dict
+
+
+(defun switch-node-text-inclusion()
+    (interactive)
+    (if (equal include-node-texts t)
+	    (setq include-node-texts nil)
+	(setq include-node-texts t)
+	)
+    (update-obr-viz)
+    )
+
+
+
+;; (define-key org-brain-visualize-mode-map "G" 'obr-viz)
+
+(setq include-node-texts t)
+
+(define-key org-brain-visualize-mode-map "N" 'switch-node-text-inclusion)
+(define-key org-brain-visualize-mode-map "R" 'obr-viz-redraw)
+
+
+
+(defun update-obr-viz ()
+    ;; (zmq-send sock (obr-viz))
+    (message (number-to-string (float-time)))
+    (setq current-config (obr-viz))
+    (if (not (equal current-config most-recent-config))
+	    (progn
+		(setq most-recent-config current-config)
+		(zmq-send sock (json-encode-alist current-config))
+		)
+	)
+    )
+
+
+
+
+
+;; (setq sock (zmq-socket (zmq-context) zmq-REQ))
+;; (zmq-connect sock "tcp://127.0.0.1:5556")
+;; (zmq-send sock "asdf")
+;; (zmq-send sock (obr-viz))
+
+;; (setq mes_back (zmq-recv sock))
+;; (zmq-connect sock "tcp://127.0.0.1:5556")
+
+
+
+
+(setq sock (zmq-socket (zmq-context) zmq-PUB))
+(zmq-bind sock "tcp://127.0.0.1:5556")
+(setq most-recent-config ())
+
+(add-hook 'org-brain-after-visualize-hook 'update-obr-viz)
+
+
+(defun obr-viz-redraw()
+    "redraw layout, either soft (apply forces to current layout) or hard (from random starting positions)"
+    (interactive)
+    (if (equal current-prefix-arg nil)
+	    (setq redraw-alist '(("redraw" . "soft")))
+	(setq redraw-alist '(("redraw" . "hard")))
+	)
+    (zmq-send sock (json-encode-alist redraw-alist))
+    )
+
+    
+
+;; * eaf sections
+;; for now use zmq for easier debugging
+;; send_to_python seems to be called now automatically through eaf-setq
+
+
+(defun eaf-obr-test ()
+  "Open EAF demo screen to verify that EAF is working properly."
+  (interactive)
+  (eaf-setq update_check 0)
+  (setq update_check_elisp 0)
+
+  
+  (eaf-setq links (obr-viz))
+  (eaf-setq update_check 0)
+  ;; (eaf-setq cur_node (org-brain-entry-at-pt))
+  (setq update_check_elisp 0)
+
+  (eaf-open "eaf-obr-test" "obr-test")
+
+  ;; (eaf--send-var-to-python)
+  )
+
+
+(defun eaf-obr-test2 ()
+  "Open EAF demo screen to verify that EAF is working properly."
+  (interactive)
+  (eaf-setq update_check 0)
+  (setq update_check_elisp 0)
+
+    (eaf-setq links (obr-viz))
+  (eaf-setq update_check 0)
+  ;; (eaf-setq cur_node (org-brain-entry-at-pt))
+  (setq update_check_elisp 0)
+
+  ;; (eaf--send-var-to-python)
+  )
+
+
+;; old redraw function
+;; (defun obr-viz-redraw()
+;;     (interactive)
+;;     """reload current edge list"""
+    
+;;     (eaf-setq update_check 1)
+;;     (setq update_check_elisp 1)
+;;     (eaf--send-var-to-python)
+
+;;     (while (eq update_check_elisp 1)
+;; 	(sit-for 0.01)
+;; 	)
+;;     (eaf-setq update_check 0)
+;;     (eaf--send-var-to-python)
+;;     (setq update_check_elisp 0)
+;;     )
+
+
+;; * scrap
+
+
+;; (remove-hook 'org-brain-after-visualize-hook 'update-obr-viz)
+;; (remove-hook 'org-brain-after-visualize-hook 'test-f)
+
+
+;; (add-hook 'org-brain-after-visualize-hook 'obr-viz-call)
+;; (remove-hook 'org-brain-after-visualize-hook 'obr-viz-call)
+
+
+;; (setq testctr 0)
+
+;; (defun test-funx ()
+;;     (setq testctr (1+ testctr))
+;;     )
+
+
+;; (add-hook 'org-brain-visualize-text-hook 'test-funx)
+;; (remove-hook 'org-brain-visualize-text-hook 'obr-viz-call)
+;; (remove-hook 'org-brain-visualize-text-hook 'test-f2)
+
+
+
+;; (defun obr-viz-call ()
+;;     "only call obr-viz if it has not been called in last 1.2 seconds, 
+;; should be changed to not work on time but on changed configuration tho"
+
+;;     (setq current-pins (mapconcat 'identity org-brain-pins ";"))
+;;     (setq current-config (concat current-pins ";" (org-brain-entry-at-pt)))
+
+;;     (if (not (equal current-config most-recent-config))
+;; 	    (progn
+;; 		(setq current-pins (mapconcat 'identity org-brain-pins ";"))
+;; 		(setq most-recent-config (concat current-pins ";" (org-brain-entry-at-pt)))
+		
+;; 		(message (concat (number-to-string (float-time)) current-config))
+;; 		(update-obr-viz)
+;; 		)
+;; 	)
+	
+;;     )
+
+;; (print link-string)
+
+;; can just send multiple things to eaf lol
+;; maybe good for adding groups later or something
+
+;; (eaf-setq update_check 0)
+;; (eaf-setq cur_node (org-brain-entry-at-pt))
+;; (setq update_check_elisp 0)
+
+;; (eaf-setq nodes node-string)
+;; (eaf-setq links link-string)
+
+;; (eaf--send-var-to-python)
+
+;; old getting children function
 ;; (defun children-specific-depth (node depth)
 ;;     """get nodes that are there"""
 ;;     ;; would be nice to also already get links: hierarchical links are here
@@ -52,67 +388,8 @@
 ;;     returns
 ;;     )
 
+;; ** some old code which i don't get anymore, probably testting or stuff
 
-(defun obvis-create-children-links (parent child)
-    """basic hierarchical function"""
-    (if (> (length parent) 4)
-	    (when (not (equal (substring parent 0 4) "cls_"))
-		(concat parent " -- " child " -- isa"))
-	(concat parent " -- " child " -- isa"))
-    )
-
-
-(defun loop-over-upper-level-nodes (parent)
-    """get nodes and hierarchical links of parent node"""
-    ;; needs both nodes and links for some tests later iirc
-    (let ((childrenx (org-brain-children parent))
-	  (res ()
-	       ))
-	(push childrenx res)
-	
-	(push (mapcar (lambda (child)
-			  (funcall #'obvis-create-children-links parent child))
-		      childrenx) res)
-	;; can also push to nodes/links to function-scoped let variables
-	res
-	))
-
-
-
-(defun children-specific-depth-let (node depth)
-    (let ((nodes-upper-level (list node))
-	  (all-nodes (list node))
-	  (all-links ())
-	  (temp-res ())
-	  (all-res ())
-	  )
-
-	;; for each depth step
-	(dotimes (i depth)
-
-	    ;; temp-res: contains 2 lists (nodes/links) for each upper-level-node
-	    ;; idk it's a bit ugly
-	    ;; but idk how to work without temp-res
-	    (push (mapcar 'loop-over-upper-level-nodes nodes-upper-level) temp-res)
-
-
-	    ;; push nodes/links to local vars
-	    (mapc (lambda (nodex) (push nodex all-nodes)) (flatten-list (mapcar 'cdr (car temp-res))))
-	    (mapc (lambda (linkx) (push linkx all-links)) (flatten-list (mapcar 'car (car temp-res))))
-	    
-	    (setq nodes-upper-level (flatten-list (mapcar 'cdr (car temp-res))))
-	    (setq temp-res ())
-
-	    )
-	(push all-links all-res)
-	(push all-nodes all-res)
-
-	all-res
-	 
-	)
-    )
-
-	
 ;; (setq res-list ())
 ;; (mapc (lambda (child) (push child res-list)) '(a b c d))
 
@@ -215,333 +492,32 @@
 ;;     friend-links
 ;;     )
 
-(defun get-friend-links (nodes)
-    """get links between nodes"""
-    (setq nodes-backup nodes)
+;; ** old way of filtering out class membership relations, now works in low-level ((concat parent child)) function
+;; of classes, don' get children relationships
+;; (if (equal (substring rel-node 0 4) "cls_")
+;; 	(progn
+;; 	    (setq node-res (children-specific-depth-let rel-node 8))
+;; 	    (push (car node-res) total-nodes)
 
-    (setq friend-links ())
-    (setq friend-nodes ())
-
-    (while nodes
-	(setq node (car nodes))
-	(setq friends (org-brain-friends node))
-	(while friends
-	    (setq friend (car friends))
-	    
-	    (setq edge-annot (org-brain-get-edge-annotation node friend))
-	    (if (not (equal edge-annot nil))
-		    (progn
-			(push (concat node " -- " friend " -- " edge-annot) friend-links)
-			(push friend friend-nodes)
-			)
-		)
-
-	    ;; check alphabetic order to avoid duplicates
-	    ;; (setq comp (car (cl-sort (list node friend) #'string-lessp)))
-	    ;; (if (eq node comp)
-	    ;; 	    (progn
-	     
-	    ;; 		(setq membership-check (member friend nodes-backup))
-	    ;; 		(if (> (len membership-check) 0)
-	    ;; 			(push (concat node " -- " friend) friend-links)
-	    ;; 		    )
-	    ;; 		)
-	    ;; 	)
-
-	    (setq friends (cdr friends))
-
-	    )
-	(setq nodes (cdr nodes))
-	)
-
-    (setq res())
-    (push friend-links res)
-    (push friend-nodes res)
-    ;; friend-links
-    res
-    )
-
-	
-
-(defun obr-viz ()
-    (interactive)
-    """main function"""
-      ;; (mapcar 'children-specific-depth org-brain-pins 3)
-    (setq rel-nodes org-brain-pins)
-      
-    (setq total-nodes ())
-    (setq total-links ())
-
-    
-    ;; get hierarchical relations
-    ;; just don't push isa relations (cdr node-res) if rel-node == cls to total links? ? 
-    (while rel-nodes
-	(setq rel-node (car rel-nodes))
-
-	;; of classes, don' get children relationships
-	;; (if (equal (substring rel-node 0 4) "cls_")
-	;; 	(progn
-	;; 	    (setq node-res (children-specific-depth-let rel-node 8))
-	;; 	    (push (car node-res) total-nodes)
-		    
-	;; 	    )
-	;;     (progn
-	;; 	(setq node-res (children-specific-depth-let rel-node 8))
-	;; 	(push (car node-res) total-nodes)
-	;; 	(push (cdr node-res) total-links)
-	;; 	)
-	;;     )
-	;; (print "total nodes")
-	;; (print total-nodes)
-
-	(setq node-res (children-specific-depth-let rel-node 8))
-	(push (car node-res) total-nodes)
-	(push (cdr node-res) total-links)
-
-	(setq rel-nodes (cdr rel-nodes))
-	)
-
-    (setq uniq-nodes-prep (counter (flatten-list total-nodes)))
-    (setq uniq-nodes (mapcar 'car uniq-nodes-prep))
-
-    ;; handle links
-    (setq friend-res (get-friend-links uniq-nodes))
-    (setq friend-links (cdr friend-res))
-    (push friend-links total-links)
-
-    ;; handle nodes
-    (setq friend-nodes (car friend-res))
-    (setq uniq-nodes (remove-duplicates (flatten-list (list uniq-nodes friend-nodes))))
-
-
-    (setq all-links (flatten-list total-links))
-
-    (setq node-string (mapconcat 'identity uniq-nodes ";"))
-    (setq link-string (mapconcat 'identity all-links ";"))
-
-    (setq node-texts (mapcar 'org-brain-text uniq-nodes))
-    (if (equal include-node-texts t)
-	(setq node-text-alist (mapcar* #'cons uniq-nodes node-texts))
-	(setq node-text-alist (mapcar* #'cons uniq-nodes (make-list (len uniq-nodes) "")))
-	)
-
-
-    (setq graph-dict
-	  `(("links" . ,all-links)
-	    ("cur_node" . ,(org-brain-entry-at-pt))
-	    ("node_texts" . ,node-text-alist)
-	    )
-	  )
-    graph-dict
-    
-
-    ;; (print link-string)
-
-    ;; can just send multiple things to eaf lol
-    ;; maybe good for adding groups later or something
-
-    ;; (eaf-setq update_check 0)
-    ;; (eaf-setq cur_node (org-brain-entry-at-pt))
-    ;; (setq update_check_elisp 0)
-
-    ;; (eaf-setq nodes node-string)
-    ;; (eaf-setq links link-string)
-
-    ;; (eaf--send-var-to-python)
-
-    )
-    
-;; data structure: dict with key node, value text
-;; is itself dict in graph_dict
-
-
-(defun obvz-text-getter(node)
-    ;; (node . (org-brain-text node))
-    (org-brain-text node)
-    )
-
-(defun switch-node-text-inclusion()
-    (interactive)
-    (if (equal include-node-texts t)
-	    (setq include-node-texts nil)
-	(setq include-node-texts t)
-	)
-    (update-obr-viz)
-    )
-
-(define-key org-brain-visualize-mode-map "N" 'switch-node-text-inclusion)
-
-	    
-	    
-	
-
-(setq include-node-texts t)
-
-
-
-;; (mapcar* #'cons '(1 2 3) '(a b c))
-
-;; (mapcar 'obvz-text-getter uniq-nodes)
-
-;; (mapcar (lambda(node)
-;; 	    (funcall #'obvz-text-getter node)) uniq-nodes)
-
-;; (mapcar (lambda (package)
-;;           (funcall #'toy-fnx (car package) (cdr package)))
-;;         packages)
-
-
-
-;; (setq all-string (mapconcat 'identity node-string link-string ""))
-
-;; (mapconcat 'identity '("foo" "bar" "baz") ", ")      
-
-
-;; watch -n 1 tail file
-
-
-;; (defun obr-viz-redraw()
-;;     (interactive)
-;;     """reload current edge list"""
-    
-;;     (eaf-setq update_check 1)
-;;     (setq update_check_elisp 1)
-;;     (eaf--send-var-to-python)
-
-;;     (while (eq update_check_elisp 1)
-;; 	(sit-for 0.01)
+;; 	    )
+;;     (progn
+;; 	(setq node-res (children-specific-depth-let rel-node 8))
+;; 	(push (car node-res) total-nodes)
+;; 	(push (cdr node-res) total-links)
 ;; 	)
-;;     (eaf-setq update_check 0)
-;;     (eaf--send-var-to-python)
-;;     (setq update_check_elisp 0)
 ;;     )
-
-(defun eaf-obr-test ()
-  "Open EAF demo screen to verify that EAF is working properly."
-  (interactive)
-  (eaf-setq update_check 0)
-  (setq update_check_elisp 0)
-
-  
-  (eaf-setq links (obr-viz))
-  (eaf-setq update_check 0)
-  ;; (eaf-setq cur_node (org-brain-entry-at-pt))
-  (setq update_check_elisp 0)
-
-  (eaf-open "eaf-obr-test" "obr-test")
-
-  ;; (eaf--send-var-to-python)
-  )
+;; (print "total nodes")
+;; (print total-nodes)
 
 
-(defun eaf-obr-test2 ()
-  "Open EAF demo screen to verify that EAF is working properly."
-  (interactive)
-  (eaf-setq update_check 0)
-  (setq update_check_elisp 0)
-
-    (eaf-setq links (obr-viz))
-  (eaf-setq update_check 0)
-  ;; (eaf-setq cur_node (org-brain-entry-at-pt))
-  (setq update_check_elisp 0)
-
-  ;; (eaf--send-var-to-python)
-  )
-
-
-
-
-;; send_to_python seems to be called now automatically through eaf-setq
-;; 
-
-;; (define-key org-brain-visualize-mode-map "G" 'obr-viz)
-(define-key org-brain-visualize-mode-map "R" 'obr-viz-redraw)
-
-
-
-(defun update-obr-viz ()
-    ;; (zmq-send sock (obr-viz))
-    (message (number-to-string (float-time)))
-    (setq current-config (obr-viz))
-    (if (not (equal current-config most-recent-config))
-	    (progn
-		(setq most-recent-config current-config)
-		(zmq-send sock (json-encode-alist current-config))
-		)
-	)
-    )
-
-
-
-
-
-;; (setq sock (zmq-socket (zmq-context) zmq-REQ))
-;; (zmq-connect sock "tcp://127.0.0.1:5556")
-;; (zmq-send sock "asdf")
-;; (zmq-send sock (obr-viz))
-
-;; (setq mes_back (zmq-recv sock))
-;; (zmq-connect sock "tcp://127.0.0.1:5556")
-
-
-(require 'zmq)
-
-(setq sock (zmq-socket (zmq-context) zmq-PUB))
-(zmq-bind sock "tcp://127.0.0.1:5556")
-(setq most-recent-config ())
-
-(add-hook 'org-brain-after-visualize-hook 'update-obr-viz)
-
-
-(defun obr-viz-redraw()
-    "redraw layout, either soft (apply forces to current layout) or hard (from random starting positions)"
-    (interactive)
-    (if (equal current-prefix-arg nil)
-	    (setq redraw-alist '(("redraw" . "soft")))
-	(setq redraw-alist '(("redraw" . "hard")))
-	)
-    (zmq-send sock (json-encode-alist redraw-alist))
-    )
-
-    
-
-
-;; (remove-hook 'org-brain-after-visualize-hook 'update-obr-viz)
-;; (remove-hook 'org-brain-after-visualize-hook 'test-f)
-
-
-;; (add-hook 'org-brain-after-visualize-hook 'obr-viz-call)
-;; (remove-hook 'org-brain-after-visualize-hook 'obr-viz-call)
-
-
-;; (setq testctr 0)
-
-;; (defun test-funx ()
-;;     (setq testctr (1+ testctr))
-;;     )
-
-
-;; (add-hook 'org-brain-visualize-text-hook 'test-funx)
-;; (remove-hook 'org-brain-visualize-text-hook 'obr-viz-call)
-;; (remove-hook 'org-brain-visualize-text-hook 'test-f2)
-
-
-
-;; (defun obr-viz-call ()
-;;     "only call obr-viz if it has not been called in last 1.2 seconds, 
-;; should be changed to not work on time but on changed configuration tho"
-
-;;     (setq current-pins (mapconcat 'identity org-brain-pins ";"))
-;;     (setq current-config (concat current-pins ";" (org-brain-entry-at-pt)))
-
-;;     (if (not (equal current-config most-recent-config))
+;; ** check alphabetic order to avoid duplicates, now replaced by directed edges
+;; (setq comp (car (cl-sort (list node friend) #'string-lessp)))
+;; (if (eq node comp)
 ;; 	    (progn
-;; 		(setq current-pins (mapconcat 'identity org-brain-pins ";"))
-;; 		(setq most-recent-config (concat current-pins ";" (org-brain-entry-at-pt)))
-		
-;; 		(message (concat (number-to-string (float-time)) current-config))
-;; 		(update-obr-viz)
+
+;; 		(setq membership-check (member friend nodes-backup))
+;; 		(if (> (len membership-check) 0)
+;; 			(push (concat node " -- " friend) friend-links)
+;; 		    )
 ;; 		)
 ;; 	)
-	
-;;     )
